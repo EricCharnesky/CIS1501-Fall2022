e7472cead87de04677980b23f7b69fb82a49351d
Eric Charnesky <eric.charnesky@careevolution.com>
2022-09-26 15:02:12 -0400

    loops

---
+++ Week5-Loops/main.py
@@ -0,0 +1,129 @@
+import random
+
+number = int(input("enter a number to count down from"))
+
+while number < 1:
+    print("Invalid entry, try again")
+    number = int(input("enter a number to count down from"))
+
+# pre-test loop
+# if the condition is true, it runs
+while number != 0:
+    print(number)
+    number -= 1
+    # when the loop body is done, go back to the top and check again
+
+print('blast off!')
+
+
+play_again = 'y'
+
+while play_again != 'n':
+    largest_value = int(input("How high of a number do you want to guess to?"))
+
+    while largest_value < 10 or largest_value > 1_000_000:
+        largest_value = int(input("How high of a number do you want to guess to?"))
+
+    magic_number = random.randint(1, largest_value)
+
+    guess = int(input(f"Guess a number 1-{largest_value}"))
+    guess_count = 1
+
+    while guess != magic_number:
+        if guess < magic_number:
+            print("Too low!")
+        else:
+            print("Too high!")
+        guess = int(input(f"Guess a number 1-{largest_value}"))
+        guess_count += 1
+
+    print(f"You guessed it in {guess_count}")
+
+    play_again = input("Do you want to play again? y/n").lower()
+
+    # validation loop - if not valid, run again
+    while play_again != 'y' and play_again != 'n':
+        play_again = input("Do you want to play again? y/n").lower()
+
+    valid_options = ['y', 'n']
+    while play_again not in valid_options:
+        play_again = input("Do you want to play again? y/n").lower()
+
+
+total_receipts = 0
+
+all_done = 'n'
+
+while all_done != 'y':
+    receipt = float(input("Enter a receipt value"))
+    total_receipts += receipt
+    all_done = input("Are you all done entering receipts? y/n")
+
+# sentinel value - invalid value with special meaning
+while receipt != 0:
+    receipt = float(input("Enter a receipt value or 0 to stop"))
+    total_receipts += receipt
+
+some_collection = [42, 7, 23, 107]
+
+# run once per value in the some_collection
+# essentially is a copy of the value
+for item in some_collection:
+    item += 42
+    # in the loop body, the current value is the variable item
+    print(item)
+
+print(some_collection)
+
+            # range gives a collection from 0 up to but not including the number
+for index in range(len(some_collection)):
+    # you can change values in a list by index
+    some_collection[index] += 42
+    print(some_collection[index])
+
+print(some_collection)
+
+some_word = input("Enter a word to get the magic value of")
+
+sum = 0
+for letter in some_word:
+    sum += ord(letter)
+
+print(f'The magic value of {some_word} is {sum}')
+
+
+classes = { "CIS 1501" : "CS1 for Data Scientists",
+            "MAT 115" : "Calculus 1",
+            "STAT 325" : "Intro to Statistics" }
+
+# when using a for loop on dictionaries, you get keys
+for item in classes:
+    print(f'key: {item} - associated value: {classes[item]}')
+
+count = 5
+while count != 0:
+
+    count -= 1
+
+# for loops can pretty much never be infinite
+for count in range(5):
+    print("Please do abc")
+
+for hour in range(24):
+    for minute in range(60):
+        for second in range(60):
+            print(f'{hour:02d}:{minute:02d}:{second:02d}')
+
+length = int(input("How long of a rectangle do you want?"))
+height = int(input("How tall of a rectangle do you want?"))
+
+for row in range(height):
+    for character in range(length):
+        print('*', end="") # doesn't go down a line
+    print() # gives a new line
+
+
+print("or the easy way")
+for row in range(height):
+    print("*" * length)
+
